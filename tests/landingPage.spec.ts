c597ff1dd07ff35269f65a3c7dd72c4e1aa4ac4f
Zbigniew Kalinowski <zbigniew.kalinowski@oke.pl>
2023-06-20 16:05:08 +0200

    additional UI Tests for Landing Page

--- tests/landingPage.spec.ts
+++ tests/landingPage.spec.ts
@@ -4,20 +4,27 @@ test('Landing page main tab', async ({ page }) => {
   await page.goto('https://teamgullit.com/');
   await expect(page).toHaveTitle(/Team Gullit/);
 
-  const news = page.getByRole('navigation').getByRole('link', { name: 'News', exact: true })
-  const team = page.getByRole('navigation').getByRole('link', { name: 'Team', exact: true })
-  const fifa23 = page.getByRole('navigation').getByRole('link', { name: 'Fifa 23', exact: true })
-  const partners = page.getByRole('navigation').getByRole('link', { name: 'Partners', exact: true })
+  function navigationItemNews() {
+    return page.getByRole('navigation').getByRole('link', { name: 'News', exact: true })
+  }
 
-  await expect(news).toBeEnabled()
-  await expect(team).toBeEnabled()
-  await expect(fifa23).toBeEnabled()
-  await expect(partners).toBeEnabled()
+  function navigationItemTeam() {
+    return page.getByRole('navigation').getByRole('link', { name: 'Team', exact: true })
+  }
 
-  news.click()
-  
+  function navigationItemFifa23() {
+    return page.getByRole('navigation').getByRole('link', { name: 'Fifa 23', exact: true })
+  }
+
+  function navigationItemPartners() {
+    return page.getByRole('navigation').getByRole('link', { name: 'Partners', exact: true })
+  }
+
+  await expect(navigationItemNews()).toBeEnabled()
+  await expect(navigationItemTeam()).toBeEnabled()
+  await expect(navigationItemFifa23()).toBeEnabled()
+  await expect(navigationItemPartners()).toBeEnabled()
+
+  navigationItemNews().click()
+  await expect(page).toHaveURL('https://www.teamgullit.com/news');
 });
-
-// test('Go to news', async ({ page }) => {
-// })
-
