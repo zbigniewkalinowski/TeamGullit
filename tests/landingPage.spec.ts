5d0fdf6c1030bc349c78aff2bc545a68bd396a0e
Zbigniew Kalinowski <zbigniew.kalinowski@oke.pl>
2023-06-20 15:32:45 +0200

    first assertions on main tab buttons

--- tests/landingPage.spec.ts
+++ tests/landingPage.spec.ts
@@ -1,8 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Landing page main tab', async ({ page }) => {
   await page.goto('https://teamgullit.com/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Team Gullit/);
+
+  const news = page.getByRole('navigation').getByRole('link', { name: 'News', exact: true })
+  const team = page.getByRole('navigation').getByRole('link', { name: 'Team', exact: true })
+  const fifa23 = page.getByRole('navigation').getByRole('link', { name: 'Fifa 23', exact: true })
+  const partners = page.getByRole('navigation').getByRole('link', { name: 'Partners', exact: true })
+
+  await expect(news).toBeEnabled()
+  await expect(team).toBeEnabled()
+  await expect(fifa23).toBeEnabled()
+  await expect(partners).toBeEnabled()
+
+  news.click()
+  
 });
+
+// test('Go to news', async ({ page }) => {
+// })
+
