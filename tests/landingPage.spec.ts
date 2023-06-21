25e521e95da9bd28eb8b5e8c2ddeb3631cd08a77
Zbigniew Kalinowski <zbigniew.kalinowski@oke.pl>
2023-06-21 10:06:59 +0200

    add UI Test for team site

--- tests/landingPage.spec.ts
+++ tests/landingPage.spec.ts
@@ -28,3 +28,25 @@ test('Landing page main tab', async ({ page }) => {
   navigationItemNews().click()
   await expect(page).toHaveURL('https://www.teamgullit.com/news');
 });
+
+test('Team page', async ({ page }) => {
+  await page.goto('https://teamgullit.com/');
+
+  function navigationItemTeam() {
+    return page.getByRole('navigation').getByRole('link', { name: 'Team', exact: true })
+  }
+
+  navigationItemTeam().click()
+  await expect(page).toHaveURL('https://www.teamgullit.com/team');
+  await expect(page.getByRole('heading', { name: 'Players' })).toHaveText('Players')
+  await expect(page.getByRole('heading', { name: 'Coaches' })).toHaveText('Coaches')
+
+  await page.getByRole('link', { name: 'Levi de Weerd NL Flag Levi de Weerd TG-LevideWeerd Socialmedia logo icon Socialmedia logo icon Socialmedia logo icon Socialmedia logo icon' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Levi de Weerd' })).toHaveText('Levi de Weerd')
+  await page.getByRole('navigation').getByRole('link', { name: 'Team', exact: true }).click();
+
+  await page.getByRole('link', { name: 'Levi de Weerd NL Flag Levi de Weerd TG-LevideWeerd Socialmedia logo icon Socialmedia logo icon Socialmedia logo icon Socialmedia logo icon' }).getByRole('link', { name: 'Socialmedia logo icon' }).first().click();
+
+  await expect(page.getByRole('link', { name: 'Levi de Weerd NL Flag Levi de Weerd TG-LevideWeerd Socialmedia logo icon Socialmedia logo icon Socialmedia logo icon Socialmedia logo icon' }).getByRole('link', { name: 'Socialmedia logo icon' }).first()).toBeEnabled()
+})
